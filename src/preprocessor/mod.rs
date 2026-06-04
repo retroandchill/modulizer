95be89b8cd4e9b8a3cce1ea5e159a9a5ff82138f
Retro & Chill <35567462+retroandchill@users.noreply.github.com>
2026-06-04 18:33:22 -0400

    Started adding the tools for parsing includes

--- src/preprocessor/mod.rs
+++ src/preprocessor/mod.rs
@@ -1,3 +1,4 @@
 mod preprocessor;
+pub mod include_syntax;
 
 pub use preprocessor::preprocess;
--- src/preprocessor/preprocessor.rs
+++ src/preprocessor/preprocessor.rs
@@ -1,18 +1,27 @@
+use std::collections::{HashMap, HashSet};
 use std::fmt::Write;
 use std::path::PathBuf;
 
-#[derive(Debug, Default)]
 pub struct PreprocessorOutput {
     pub source: String,
     
 }
 
+#[derive(Default)]
+struct PreprocessorState {
+    source: String,
+    included: HashSet<PathBuf>,
+    definitions: HashMap<String, String>,
+}
+
 pub fn preprocess(source: &str, include_paths: &[PathBuf]) -> anyhow::Result<PreprocessorOutput> {
-    let mut result = PreprocessorOutput::default();
+    let mut result = PreprocessorState::default();
 
     for line in source.lines() {
         result.source.write_str(line)?;
     }
 
-    Ok(result)
+    Ok(PreprocessorOutput {
+        source: result.source,
+    })
 }
