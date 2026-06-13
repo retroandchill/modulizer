669181129fb463522370fde5590a4b348a4c6084
Retro & Chill <35567462+retroandchill@users.noreply.github.com>
2026-06-13 15:23:56 -0400

    Added Catch2 tests for the C++ bindings

---
+++ bindings/cpp/tests/builder_test.cpp
@@ -0,0 +1,12 @@
+//
+// Created by fcors on 6/13/2026.
+//
+#include <catch2/catch_test_macros.hpp>
+import modulizer;
+
+using namespace modulizer;
+
+TEST_CASE("Can create builder", "[builder]") {
+  OptionsBuilder builder;
+  auto options = builder.name("test").build();
+}
